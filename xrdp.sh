6dcc8f5d0c4923e69fca3fd0de82b0831c7569e8
riskirahmand <56805258+riskirahmand@users.noreply.github.com>
2019-10-21 20:04:32 +0700

    Add files via upload

---
+++ xrdp.sh
@@ -0,0 +1,6 @@
+sudo apt-get update -y 
+sudo apt-get install -y xfce4 xfce4-goodies
+sudo apt-get install -y firefox
+sudo apt-get install -y xrdp
+echo xfce4-session >~/.xsession
+sudo service xrdp start
